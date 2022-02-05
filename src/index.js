8cdfec096f62b2366bd418fe742bb82ae7048d0e
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-02-05 18:40:25 +0100

    Updated the format

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 // Import packages
 const express = require('express');
 const winston = require('winston');
+const { combine, timestamp, printf } = winston.format;
+
+let request;
 
 const app = express();
 
 const consoleTransport = new winston.transports.Console();
 
 const myWinstonOptions = {
-    transports: [consoleTransport]
+    transports: [consoleTransport],
+    format: combine(
+        timestamp(),
+        printf(info => {
+            const { req, res } = info.message;
+            return `${ info.timestamp } ${ info.level }: ${ request.hostname }${ request.port || ''}${ request.originalUrl }`;
+        })
+    )
 };
+
+// ${ req.hostname }${ req.port || ''}${ req.originalUrl }
 
 const logger = new winston.createLogger(myWinstonOptions);
 
 function logRequest(req, res, next) {
-    logger.info(req.url);
+    request = req;
+    logger.info(req);
     next();
 }
 app.use(logRequest);
 
 function logError(err, req, res, next) {
-    logger.error(err);
+    logger.error(req);
     next();
 }
 app.use(logError);
